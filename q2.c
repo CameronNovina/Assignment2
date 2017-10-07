534d257da59eb8819cd650c95f71795ebe1614b1
Murg <cnovina97@hotmail.com>
2017-10-07 17:35:04 -0400

    First Commit

---
+++ q2.c
@@ -0,0 +1,14 @@
+/* Assignment 1: Question 1
+ * September 13, 2017 
+ * Cameron Novina, 991400962
+ * User specifies the number of digits and a range of numbers that have
+ * that specified number of digits. The program will then print any
+ * number that qualifies as an "Armstrong"
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+ 
+}
